07fe262317b71866d5c8d15cf75e37155fbb3e0a
Bruno Mota <bruno7motadev@gmail.com>
2024-03-31 00:52:34 -0300

    JS script fix

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ checkoutBtn.addEventListener("click", function() {
         }).join("")
     
         const message = encodeURIComponent(cartItems)
-        const phone = "98991002577"
+        const phone = ""
     
         window.open(`https://wa.me/${phone}?text=${message} Endereço: ${addressInput.value}`, "_blank")
     
